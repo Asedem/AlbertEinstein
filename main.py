83e0d301563e92b01c7b4e28f53dae0d8acb6a10
Asedem <simon.oberzier@juniorjob.de>
2022-10-23 21:22:48 +0200

    Fixed Collision bug and set the application Icon

--- main.py
+++ main.py
@@ -2,18 +2,26 @@ import pygame
 
 # Insert the important global variables
 
-SPEED = 10
+FPS = 240
+SPEED = 3
 SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = (800, 800)
 FIGURE_SIZE = FIGURE_WIDTH, FIGURE_HEIGHT = (100, 100)
 TEXT_COLOR = (27, 27, 27)
 BACKGROUND_COLOR = (27, 207, 107)
 CAPTION = 'Albert Einstein zur Zeit des dritten Reiches'
 
+SCREEN_CENTER = SCREEN_CENTER_X, SCREEN_CENTER_Y = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 EINSTEIN_POSITION = EINSTEIN_POS_X, EINSTEIN_POS_Y = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 # Init the Pygame Python library that is used for the UI
 
 pygame.init()
+fps_clock = pygame.time.Clock()
+
+# Set the icon of the application
+
+icon = pygame.image.load('images/AlbertEinstein.png')
+pygame.display.set_icon(icon)
 
 # Create the Window that displays the Game
 
@@ -30,18 +38,18 @@ text = font.render('Jahr: 1922', True, TEXT_COLOR)
 screen.blit(text, (EINSTEIN_POS_X - text.get_width() / 2, EINSTEIN_POS_Y - text.get_height() / 2))
 
 continue_text = font.render('Drücke LEHRTASTE um fortzufahren:', True, TEXT_COLOR)
-screen.blit(continue_text, (EINSTEIN_POS_X - continue_text.get_width() / 2, (EINSTEIN_POS_Y - continue_text.get_height() / 2) + 300))
+screen.blit(continue_text, (SCREEN_CENTER_X - continue_text.get_width() / 2, (SCREEN_CENTER_Y - continue_text.get_height() / 2) + 300))
 
 pygame.display.update()
 
-running = True
+running1 = True
 
-while running:
+while running1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            running = False
+            running1 = False
 
 # Create the Sprite for Albert Einstein
 
@@ -55,9 +63,9 @@ letter_image = pygame.transform.scale(pygame.image.load('images/Letter.png'), tu
 letter_sprite = letter_image.get_rect()
 letter_sprite.center = FIGURE_SIZE
 
-running = True
+running2 = True
 
-while running:
+while running2:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -82,45 +90,48 @@ while running:
     screen.blit(einstein_image, einstein_sprite)
     pygame.display.update()
 
-    if pygame.Rect.collidedict(letter_sprite, einstein_sprite):
-        running = False
+    if einstein_sprite.colliderect(letter_sprite):
+        running2 = False
+
+    fps_clock.tick()
 
 pygame.draw.rect(screen, BACKGROUND_COLOR, (0, 0, SCREEN_WIDTH, SCREEN_HEIGHT))
 text = font.render('Drücke LEHRTASTE um fortzufahren:', True, TEXT_COLOR)
+einstein_sprite.center = SCREEN_CENTER
 
 speach_image = pygame.image.load('images/Nachricht.png')
 speach_sprite = speach_image.get_rect()
-speach_sprite.center = (EINSTEIN_POS_X + 200, EINSTEIN_POS_Y - 125)
+speach_sprite.center = (SCREEN_CENTER_X + 200, SCREEN_CENTER_Y - 125)
 
-screen.blit(continue_text, (EINSTEIN_POS_X - text.get_width() / 2, (EINSTEIN_POS_Y - text.get_height() / 2) + 300))
+screen.blit(continue_text, (SCREEN_CENTER_X - text.get_width() / 2, (SCREEN_CENTER_Y - text.get_height() / 2) + 300))
 screen.blit(einstein_image, einstein_sprite)
 screen.blit(speach_image, speach_sprite)
 pygame.display.update()
 
-running = True
+running3 = True
 
-while running:
+while running3:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            running = False
+            running3 = False
 
 # Display the Sprite for Adolf Hitler
 
 hitler_image = pygame.transform.scale(pygame.image.load('images/AdolfHitler.png'), FIGURE_SIZE)
 hitler_sprite = hitler_image.get_rect()
-hitler_sprite.center = EINSTEIN_POSITION
+hitler_sprite.center = SCREEN_CENTER
 
 # GameLoop for the First Sequence
 
-running = True
+running4 = True
 
-while running:
+while running4:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            running4 = False
 
         keys = pygame.key.get_pressed()
 
@@ -140,5 +151,7 @@ while running:
     screen.blit(einstein_image, einstein_sprite)
     screen.blit(hitler_image, hitler_sprite)
     pygame.display.update()
+
+    fps_clock.tick()
 
 pygame.quit()
